df1f436a80b194fc4b075d6865d38eadafe781cf
SequenceK <Omar2281996>
2019-01-30 19:16:40 +0400

    All ionic files

---
+++ src/pages/home/home.ts
@@ -0,0 +1,74 @@
+import { Component, NgZone } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { BLE } from '@ionic-native/ble';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  
+  devices: any[] = [];
+  statusMessage: string;
+
+  constructor(public navCtrl: NavController, 
+              private toastCtrl: ToastController,
+              private ble: BLE,
+              private ngZone: NgZone) { 
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+    this.scan();
+  }
+
+  scan() {
+    this.setStatus('Scanning for Bluetooth LE Devices');
+    this.devices = [];  // clear list
+
+    this.ble.scan([], 2).subscribe(
+      device => this.onDeviceDiscovered(device), 
+      error => this.scanError(error)
+    );
+
+    setTimeout(this.setStatus.bind(this), 5000, 'Scan complete');
+  }
+
+  hexpad(num:number): string {
+    let s = num.toString(16);
+    while (s.length < 2) s = s + "0";
+    return s;
+  }
+
+
+  onDeviceDiscovered(device) {
+    var adData = new Uint8Array(device.advertising); 
+    var id = "";
+    adData.slice(1,9).forEach(this.hexpad);
+    console.log('AdData ' + adData);
+    console.log('Discovered ' + JSON.stringify(device, null, 2));
+    this.ngZone.run(() => {
+      this.devices.push(device);
+    });
+  }
+
+  // If location permission is denied, you'll end up here
+  scanError(error) {
+    this.setStatus('Error ' + error);
+    let toast = this.toastCtrl.create({
+      message: 'Error scanning for Bluetooth low energy devices',
+      position: 'middle',
+      duration: 5000
+    });
+    toast.present();
+  }
+
+  setStatus(message) {
+    console.log(message);
+    this.ngZone.run(() => {
+      this.statusMessage = message;
+    });
+  }
+
+}
